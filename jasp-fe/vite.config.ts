2476a9451aefecc4ef66b60639ff56243d9e775d
Martin Šmídl <smidl.martin1@gmail.com>
2024-04-23 10:26:07 +0200

    Add shadUI

--- jasp-fe/vite.config.ts
+++ jasp-fe/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/japs/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     open: true
   }
