d7549e1dd8e51249cec69898a2592488f56fdd86
David Tejzr <david.tejzr@atreo.cz>
2024-05-06 18:51:37 +0200

    feat: tanstack query

--- japs-fe/src/App.tsx
+++ japs-fe/src/App.tsx
@@ -2,16 +2,20 @@
 import { NextUIProvider } from '@nextui-org/react'
 import { Outlet, useNavigate } from 'react-router-dom'
 import { Navigation } from './components/Navigation'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 function App() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const queryClient = new QueryClient()
   return (
     <>
       <NextUIProvider navigate={navigate}>
-        <main className='dark text-foreground bg-background'>
-          <Navigation />
-          <Outlet />
-        </main>
+          <QueryClientProvider client={queryClient}>
+            <main className='dark text-foreground bg-background'>
+              <Navigation />
+              <Outlet />
+            </main>
+          </QueryClientProvider>
       </NextUIProvider>
     </>
   )
